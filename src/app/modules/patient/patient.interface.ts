27b750fd6225c141ee4d3ddeb0374680aab191df
tasniaarhamarham <tasniaarhamarham@gmail.com>
2026-02-24 23:28:04 +0600

    my user service has done

---
+++ src/app/modules/patient/patient.interface.ts
@@ -0,0 +1,48 @@
+import type { BloodGroup, Gender } from "../../../generated/prisma/enums";
+
+export interface UserProfileInfo {
+  name?: string;
+  image?: string;
+}
+
+export interface PatientProfileInfo {
+  name?: string;
+  profilePhoto?: string;
+  contactNumber?: string;
+  address?: string;
+}
+
+export interface PatientHealthDataInfo {
+  gender: Gender;
+  dateOfBirth: string;
+  bloodGroup: BloodGroup;
+  hasAllergies?: boolean;
+  hasDiabetes?: boolean;
+  height: string;
+  weight: string;
+  smokingStatus?: boolean;
+  dietaryPreferences?: string;
+  pregnancyStatus?: boolean;
+  mentalHealthHistory?: string;
+  immunizationStatus?: string;
+  hasPastSurgeries?: boolean;
+  recentAnxiety?: boolean;
+  recentDepression?: boolean;
+  maritalStatus?: string;
+  patientId?: string;
+}
+
+export interface reportInfo {
+  reportName?: string;
+  reportLink?: string;
+  shouldDelete?: boolean;
+  reportId?:string
+}
+
+
+export interface IUpdatePatientProfile {
+    user:UserProfileInfo,
+    patientInfo:PatientProfileInfo,
+    patientHealth:PatientHealthDataInfo
+    medicalReport:reportInfo[]
+}
---
+++ src/app/modules/patient/patient.service.ts
@@ -0,0 +1,85 @@
+import type { JwtPayload } from "jsonwebtoken";
+import type { IUpdatePatientProfile } from "./patient.interface";
+import { prisma } from "../../lib/prisma";
+import { AppError } from "../../errorHelplers/appError";
+import { deleteFileFromCloudinary } from "../../../config/cloude.config";
+
+const updatePatientProfile = async (
+  authUser: JwtPayload,
+  payload: IUpdatePatientProfile
+) => {
+  const { user, patientInfo, patientHealth, medicalReport } = payload;
+  const { patientId, ...healthData } = patientHealth;
+
+
+  const isUserExist = await prisma?.user?.findUniqueOrThrow({
+    where: { email: authUser?.email, isDeleted: false },
+  });
+
+  const isPatientExist = await prisma.patient.findUniqueOrThrow({
+    where: { email: authUser?.email, isDeleted: false },
+  });
+
+  if (!isUserExist || !isPatientExist) {
+    throw new AppError(404, "User not found");
+  }
+
+  const result = await prisma.$transaction(async (tx) => {
+    const updateUser = await tx.user.update({
+      where: { id: isUserExist?.id },
+      data: { ...user },
+    });
+    if (updateUser) {
+      const updatePatient = await tx.patient.update({
+        where: { id: isPatientExist?.id },
+        data: { ...patientInfo },
+      });
+      if (updatePatient) {
+        const updatePatientHealth = await tx.patientHealthData.upsert({
+          where: { patientId: updatePatient.id },
+          update: {
+            ...healthData,
+          },
+          create: {
+            ...healthData,
+            patientId: updatePatient?.id,
+          },
+        });
+
+        if (medicalReport && medicalReport?.length > 0) {
+          const reportsToDelete = medicalReport.filter((r) => r?.shouldDelete);
+          for (const report of reportsToDelete) {
+            if (report?.reportId) {
+              const deletedReport = await tx.medicalReport.delete({
+                where: { id: report.reportId },
+              });
+              if (deletedReport?.reportLink) {
+                await deleteFileFromCloudinary(deletedReport.reportLink);
+              }
+            }
+          }
+
+          const reportsToCreate = medicalReport
+            .filter((r) => !r?.shouldDelete)
+            .map((r) => ({
+              reportLink: r?.reportLink as string,
+              reportName: r?.reportName as string,
+              patientId: updatePatient.id,
+            }));
+       
+          if (reportsToCreate.length) {
+            await tx.medicalReport.createMany({
+              data: reportsToCreate 
+            });
+          }
+        }
+      }
+    }
+  });
+
+  return result;
+};
+
+export const patientService = {
+  updatePatientProfile,
+};
